12c5f7e04ed162225f5a5da4be9e00309f2e6d3f
NoahTX <noah@transifex.com>
2015-04-05 20:46:37 -0700

    added exercise 1 from lpthw, .gitignore, and the sample file.

---
+++ ex1.py
@@ -0,0 +1,18 @@
+"""
+Learn Python the Hard Way
+Exercise 1
+This is one way to do a comment
+"""
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# this is a comment as well
+# study drills
+print "another line"
+
